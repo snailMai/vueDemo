5518688de6ac89dd083afd586780ea998c6c79cf
snailmai <1033479811@qq.com>
2021-04-21 22:57:15 +0800

    实现vue element UI,包括解决"套娃"的问题

--- src/router/index.js
+++ src/router/index.js
@@ -1,35 +1,75 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-import TestUser from '../views/TestUser.vue'
+// import TestUser from '../views/TestUser.vue'
+import App from "../App"
+import Index from "../views/index"
 
 Vue.use(VueRouter)
 
 const routes = [
+
+  {
+    path: '/',
+    name: '导航一App',
+    component: Index,
+    children: [
+      {
+        path: 'TestUser',
+        name: 'TestUser',
+        component: () => import('../views/TestUser')
+      },
+      {
+        path: '/about',
+        name: 'About',
+        component: () => import('../views/About')
+      }
+    ]
+  },
+
+
   {
     path: '/',
     name: 'Home',
     component: Home  // Home = '../views/Home.vue'
   },
-    // 上边基于vue2.0,下边基于3.0(懒加载)
+  //  用于测试重复
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/',
+    name: '导航二App',
+    component: App,
+    children: [
+      {
+        path: 'TestUser',
+        name: 'TestUser',
+        component: () => import('../views/TestUser')
+      },
+      {
+        path: '/about',
+        name: 'About',
+        component: () => import('../views/About')
+      }
+    ]
   },
+    // 上边基于vue2.0,下边基于3.0(懒加载)
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // },
     //TestUser
   // {
   //   path: '/TestUser',
   //   component: TestUser
   // }
 
-  {
-    path: '/TestUser',
-    component: () => import('../views/TestUser')
-  }
+  // {
+  //   path: '/TestUser',
+  //   component: () => import('../views/TestUser')
+  // }
 
 
 ]
---
+++ src/views/index.vue
@@ -0,0 +1,81 @@
+<template>
+    <el-container style="height: 500px; border: 1px solid #eee">
+        <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
+
+            <!--        固定方式-->
+            <!--        <el-menu :default-openeds="['1']" :default-active="'1-1'">-->
+
+            <!--&lt;!&ndash;          只到二级菜单&ndash;&gt;-->
+            <!--          <el-submenu index="1">-->
+            <!--            <template slot="title"><i class="el-icon-message"></i>导航一</template>-->
+            <!--            <el-menu-item index="1-1">选项1</el-menu-item>-->
+            <!--            <el-menu-item index="1-2">选项2</el-menu-item>-->
+            <!--          </el-submenu>-->
+
+            <!--          <el-submenu index="2">-->
+            <!--            <template slot="title"><i class="el-icon-menu"></i>导航二</template>-->
+            <!--            <el-menu-item index="2-1">选项1</el-menu-item>-->
+            <!--            <el-menu-item index="2-2">选项2</el-menu-item>-->
+            <!--          </el-submenu>-->
+
+            <!--          &lt;!&ndash;          只到三级菜单&ndash;&gt;-->
+            <!--          <el-submenu index="3">-->
+            <!--            <template slot="title"><i class="el-icon-message"></i>导航三</template>-->
+            <!--            <el-menu-item index="3-1">选项1</el-menu-item>-->
+            <!--            <el-menu-item index="3-2">选项2</el-menu-item>-->
+            <!--            <el-submenu index="3-3">-->
+            <!--              <template slot="title"><i class="el-icon-setting"></i>选项3</template>-->
+            <!--              <el-menu-item index="3-3-1">选项3-1</el-menu-item>-->
+            <!--              <el-menu-item index="3-3-2">选项3-2</el-menu-item>-->
+            <!--            </el-submenu>-->
+            <!--          </el-submenu>-->
+
+            <!--        </el-menu>-->
+
+            <!--        自动生成aside-->
+            <el-menu>
+                <el-submenu v-for="(item, index) in $router.options.routes" :index="index+''">
+                    <template slot="title"><i class="el-icon-message"></i>{{item.name}}</template>
+                    <el-menu-item v-for="(item2,index2) in item.children" :index="index+'-'+index2">{{item2.name}}</el-menu-item>
+
+                </el-submenu>
+            </el-menu>
+        </el-aside>
+
+        <el-container>
+            <el-header style="text-align: right; font-size: 12px">
+                <el-dropdown>
+                    <i class="el-icon-setting" style="margin-right: 15px"></i>
+                    <el-dropdown-menu slot="dropdown">
+                        <el-dropdown-item>查看</el-dropdown-item>
+                        <el-dropdown-item>新增</el-dropdown-item>
+                        <el-dropdown-item>删除</el-dropdown-item>
+                    </el-dropdown-menu>
+                </el-dropdown>
+                <span>王小虎</span>
+            </el-header>
+
+            <el-main>
+                <router-view></router-view>
+                <!--          <el-table :data="tableData">-->
+                <!--            <el-table-column prop="date" label="日期" width="140">-->
+                <!--            </el-table-column>-->
+                <!--            <el-table-column prop="name" label="姓名" width="120">-->
+                <!--            </el-table-column>-->
+                <!--            <el-table-column prop="address" label="地址">-->
+                <!--            </el-table-column>-->
+                <!--          </el-table>-->
+            </el-main>
+        </el-container>
+    </el-container>
+</template>
+
+<script>
+    export default {
+        name: "index"
+    }
+</script>
+
+<style scoped>
+
+</style>
